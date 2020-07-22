30f43a99435d758b4f511d3dffe3410fefc9a46b
Elementrixi <67051385+Elementrixi@users.noreply.github.com>
2020-07-22 20:59:03 +0300

    executor added

---
+++ src/main/java/com/alpashaev/executor/Executor.java
@@ -0,0 +1,9 @@
+package main.java.com.alpashaev.executor;
+
+import main.java.com.alpashaev.memory.OutOfMemory;
+
+public class Executor {
+    public static void solutionHomework() {
+        OutOfMemory.noMemory();
+    }
+}
